202fadd2038e2deb4e90affb077e3f991e1276e7
shangboyang <yang8701@gmail.com>
2016-04-01 16:55:13 +0800

    update

--- src/Main.vue
+++ src/Main.vue
@@ -1,14 +1,5 @@
 <template>
-  <div id="app">
-    <h1>Hello App!</h1>
-    <p>
-      <!-- use v-link directive for navigation. -->
-      <a v-link="{ path: '/foo' }">Go to Foo</a>
-      <a v-link="{ path: '/bar' }">Go to Bar</a>
-    </p>
-    <!-- use router-view element as route outlet -->
-    <router-view></router-view>
-  </div>
+  
 </template>
 
 <script>
