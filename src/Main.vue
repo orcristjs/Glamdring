4ecc58be77b94ddec1f6aa1b6c975620da5df48c
shangboyang <yang8701@gmail.com>
2016-04-01 16:37:14 +0800

    update

---
+++ src/Main.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1>Hello App!</h1>
+    <p>
+      <!-- use v-link directive for navigation. -->
+      <a v-link="{ path: '/foo' }">Go to Foo</a>
+      <a v-link="{ path: '/bar' }">Go to Bar</a>
+    </p>
+    <!-- use router-view element as route outlet -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
