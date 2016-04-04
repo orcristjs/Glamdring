31336956670bd0e68babbc692872f8c8108ee151
shangboyang <yang8701@gmail.com>
2016-04-04 21:17:24 +0800

    vue-router

--- src/app.js
+++ src/app.js
@@ -18,11 +18,19 @@ var Hello = Vue.extend({
 });
 
 var Foo = Vue.extend({
-    template: '<p>This is foo!</p>'
+    template:
+    '<div class="foo">' +
+        '<h2>This is Foo!</h2>' +
+        '<router-view></router-view>' + // <- 嵌套的外链
+    '</div>'
 });
 
 var Bar = Vue.extend({
     template: '<p>This is bar!</p>'
+});
+
+var Baz = Vue.extend({
+    template: '<p>This is baz!</p>'
 });
 
 // The router needs a root component to render.
@@ -42,7 +50,26 @@ var router = new VueRouter();
 // We'll talk about nested routes later.
 router.map({
     '/foo': {
-        component: Foo
+        component: Foo,
+        // 在/foo下设置一个子路由
+        subRoutes: {
+            '/': {
+                // 当匹配到 /foo 时，这个组件会被渲染到 Foo 组件的 <router-view> 中。
+                // 为了简便，这里使用了一个组件的定义
+                component: {
+                    template: '<p>Default sub view for Foo</p>'
+                }
+            },
+            '/bar': {
+                // 当匹配到/foo/bar时，会在Foo's <router-view>内渲染
+                // 一个Bar组件
+                component: Bar
+            },
+            '/baz': {
+                // Baz也是一样，不同之处是匹配的路由会是/foo/baz
+                component: Baz
+            }
+        }
     },
     '/bar': {
         component: Bar
