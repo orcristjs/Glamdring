47e30ca285979a82d88582992007c022b3c07eb9
shangboyang <yang8701@gmail.com>
2016-04-05 00:26:48 +0800

    afterEach

--- src/app.js
+++ src/app.js
@@ -128,4 +128,7 @@ router.afterEach(function ({ to, from }) {
 });
 
 
+// Now we can start the app!
+// The router will create an instance of App and mount to
+// the element matching the selector #app.
 router.start(App, '#app');
